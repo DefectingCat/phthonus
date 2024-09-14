1d0228d5e5019712577ec7628ff429f69117196a
xfy <xfyxfy@proton.me>
2024-09-14 14:58:13 +0800

    init with tokio

--- web/Rust/axum/src/main.rs
+++ web/Rust/axum/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy::dotenv;
+use tracing::info;
+use utils::init_logger;
+
+mod utils;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    init_logger();
+    info!("Hello, world!");
 }
