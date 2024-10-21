602c7f0fde032cbd097f3b277345b72334a56666
xfy <xfyxfy@proton.me>
2024-10-21 09:54:02 +0800

    feat(axum): update route logging middleware

--- frameworks/Rust/axum/src/middlewares/mod.rs
+++ frameworks/Rust/axum/src/middlewares/mod.rs
@@ -40,30 +40,32 @@ pub async fn add_version(
 /// This middleware will calculate each request latency
 /// and add request's information to each info_span.
 pub fn logging_route(router: Router) -> Router {
-    router.layer(
-        TraceLayer::new_for_http()
-            .make_span_with(|req: &Request<_>| {
-                let unknown = &HeaderValue::from_static("Unknown");
-                let empty = &HeaderValue::from_static("");
-                let headers = req.headers();
-                let ua = headers
-                    .get("User-Agent")
-                    .unwrap_or(unknown)
-                    .to_str()
-                    .unwrap_or("Unknown");
-                let host = headers.get("Host").unwrap_or(empty).to_str().unwrap_or("");
-                info_span!("HTTP", method = ?req.method(), host, uri = ?req.uri(), ua)
-            })
-            .on_request(|_req: &Request<_>, _span: &Span| {})
-            .on_response(|res: &Response, latency: Duration, _span: &Span| {
-                info!("{} {}μs", res.status(), latency.as_micros());
-            })
-            .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {})
-            .on_eos(|_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {})
-            .on_failure(
-                |error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
-                    error!("{}", error);
-                },
-            ),
-    )
+    let make_span = |req: &Request<_>| {
+        let unknown = &HeaderValue::from_static("Unknown");
+        let empty = &HeaderValue::from_static("");
+        let headers = req.headers();
+        let ua = headers
+            .get("User-Agent")
+            .unwrap_or(unknown)
+            .to_str()
+            .unwrap_or("Unknown");
+        let host = headers.get("Host").unwrap_or(empty).to_str().unwrap_or("");
+        info_span!("HTTP", method = ?req.method(), host, uri = ?req.uri(), ua)
+    };
+
+    let trace_layer = TraceLayer::new_for_http()
+        .make_span_with(make_span)
+        .on_request(|_req: &Request<_>, _span: &Span| {})
+        .on_response(|res: &Response, latency: Duration, _span: &Span| {
+            info!("{}", format_latency(latency, res.status()));
+        })
+        .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {})
+        .on_eos(|_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {})
+        .on_failure(
+            |error: ServerErrorsFailureClass, latency: Duration, _span: &Span| {
+                error!("{}", format_latency(latency, error));
+            },
+        );
+
+    router.layer(trace_layer)
 }
